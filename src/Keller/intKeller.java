d294ad6d0da1316aade18f45dfc625ec59570488
charlotte.hoelscher <charlotte.hoelscher@urs-os.de>
2023-11-02 09:00:33 +0100

    ...

---
+++ src/Keller/intKeller.java
@@ -0,0 +1,17 @@
+package Keller;
+
+public interface intKeller {
+
+        // liefert true, falls Keller leer, sonst false
+        public boolean isEmpty();
+
+        // legt int-Wert x auf den Keller
+        public void push(int x);
+
+        // liefert das oberste Kellerelement
+        public int peek();
+
+        // löscht das oberste Kellerelement
+        public int pop();
+    }
+
---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import Keller.Verschiebebahnhof;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Verschiebebahnhof bhf = new Verschiebebahnhof();
+
+        System.out.println(bhf);
+        bhf.sortieren();
+        System.out.println(bhf);
+
+    }
+}
