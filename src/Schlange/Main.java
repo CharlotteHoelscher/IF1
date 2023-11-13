4a151f33ccaef43584a72adec1686900f5177d0f
charlotte.hoelscher <charlotte.hoelscher@urs-os.de>
2023-11-13 11:12:03 +0100

    ...

---
+++ src/Schlange/Main.java
@@ -0,0 +1,16 @@
+package Schlange;
+
+public class Main {
+
+    public static void main (String[] args) {
+
+        intArraySchlange s= new intArraySchlange();
+
+        s.enqueue(32);
+        s.enqueue(16);
+
+        System.out.println(s);
+    }
+
+
+}
